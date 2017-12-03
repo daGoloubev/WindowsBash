0828d0211de5a477db7777200863a0e0218d6426
Daniel Goloubev <danielgoloubev@gmail.com>
2017-12-03 20:19:34 +0100

    Version 1.0 of VLM script

---
+++ VLM/movefiles.cmd
@@ -0,0 +1,28 @@
+:: ========================================================================= ::
+:: Script for moving files.
+:: Example string: vlm-kmvfe-2187
+:: @author: Daniel Rasmussen
+:: ======================================================================== ::
+@ECHO off
+SETLOCAL ENABLEDELAYEDEXPANSION
+::SET "sourcedir=C:\Users\Daniel\batch\GenFiles"
+::SET "destdir=C:\Users\Daniel\batch\GenFiles\vlm"
+SET sourcedir=%~dp0
+SET destdir=%sourcedir%vlm
+
+SET MAX=100
+FOR /f "delims=- tokens=1-3" %%a IN (
+    'dir /b /a-d "%sourcedir%\vlm-*-*.*"'
+  ) DO IF "%%c" neq "" (
+    SET mid=%%b
+    SET pid=!mid:~0,3!
+    SET id=!mid:~3!
+    SET last=%%c
+    SET nbr=!last:~0,-5!
+    SET /a res = !nbr! / !MAX!
+    SET /a res = !res! * !MAX!
+    MD "%destdir%\!pid!\!id!\!res!\"
+    MOVE "%sourcedir%\%%a-%%b-%%c" "%destdir%\!pid!\!id!\!res!\%%a-%%b-%%c"
+  )
+
+GOTO :EOF
